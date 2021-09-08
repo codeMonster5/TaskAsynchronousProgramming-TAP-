40d9da31ad9c8ec678d50946b7fd4bb215958067
Mani <mani.bhushan@nowfloats.com>
2021-09-09 00:04:05 +0530

    Add project files.

---
+++ TaskAsynchronousProgramming(TAP)/Driver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskAsynchronousProgramming_TAP_
+{
+    public class Driver
+    {
+        public async Task BackToOffice()
+        {
+            await Task.Delay(5000);
+
+           
+       
+        }
+
+        public async Task LoadVerify()
+        {
+            await Task.Delay(5000);
+
+            
+            
+        }
+
+        public async Task TaskAssign()
+        {
+            await Task.Delay(5000);
+
+            
+            
+        }
+
+    }
+}
---
+++ TaskAsynchronousProgramming(TAP)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskAsynchronousProgramming_TAP_
+{
+    public  class Program
+    {
+        static async Task  Main(string[] args)
+        {
+            // Driver Load Distributor Model
+
+            // Driver BACK TO OFFICE
+
+            // Verify the load
+
+            // Task assign to Driver
+
+            Driver dr = new Driver();
+            DateTime time = DateTime.Now;
+            var task1 = dr.BackToOffice();
+            var task2= dr.LoadVerify();
+            var task3 = dr.TaskAssign();
+
+           
+
+            Console.WriteLine("mani");
+
+
+            Console.WriteLine(DateTime.Now.Subtract(time).TotalSeconds);
+        }
+    }
+}
